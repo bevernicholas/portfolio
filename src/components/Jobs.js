d88716a84a36a75e905b1ec348bf1c58162be4ba
Nick Bever <nicholasbever@Nicholass-MacBook-Air-2.local>
2020-12-31 11:02:29 -0800

    connected front end to strapi back end. creating jobs component

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,11 +1,46 @@
-import React from "react"
+import React, {useState} from "react"
 import Title from "./Title"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJobs(sort: {fields: strapiId, order: DESC}) {
+      nodes {
+        strapiId
+        company
+        date
+        desc {
+          id
+          name
+        }
+        position
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const {
+    allStrapiJobs: { nodes:jobs },
+  } = useStaticQuery(query);
+  const uniqueJobs = [...new Set(jobs.map(jobs => jobs.company))];
+
+  const [value, setValue] = useState(0);
+  const {company, position, date, desc} = jobs[value];
+
+  return <section className="section jobs">
+    <Title title="experience" />
+    <div className = "jobs-center">
+      {/* btn container */}
+      <div className="btn-container">
+        {uniqueJobs.map((item, index) => {
+          return <button key={index}>{item}</button>
+        })}
+      </div>
+    </div>
+  </section>
 }
 
 export default Jobs
